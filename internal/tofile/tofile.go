7fc7dfff834a23e136c646a04f7ada69d8f0e2f4
Richard Weston <github@riweston.io>
2024-07-12 11:14:19 +0100

    refactor: Refactored the file writer to implement the io.Writer interface for testing

--- internal/tofile/tofile.go
+++ internal/tofile/tofile.go
@@ -2,8 +2,8 @@
 package tofile
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -16,6 +16,62 @@ const (
 	startMarker    = "<!--START_SECTION:go-credly-->"
 	endMarker      = "<!--END_SECTION:go-credly-->"
 )
+
+// BadgeWriter is a struct that writes the badges to a file
+type BadgeWriter struct {
+	writer io.Writer
+}
+
+// NewBadgeWriter creates a new instance of the BadgeWriter struct with the provided io.Writer
+func NewBadgeWriter(w io.Writer) *BadgeWriter {
+	return &BadgeWriter{writer: w}
+}
+
+// WriteBadges writes the badges to the io.Writer
+func (bw *BadgeWriter) WriteBadges(badges []fetch.FilteredBadge) error {
+	content := generateBadgeContent(badges)
+	_, err := fmt.Fprintf(bw.writer, defaultContent, content)
+	return err
+}
+
+// UpdateContent updates the content between the markers in the io.Writer with the provided badges
+func (bw *BadgeWriter) UpdateContent(r io.Reader, badges []fetch.FilteredBadge) error {
+	existingContent, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	updatedContent := replaceContentBetweenMarkers(string(existingContent), badges)
+	_, err = bw.writer.Write([]byte(updatedContent))
+	return err
+}
+
+func generateBadgeContent(badges []fetch.FilteredBadge) string {
+	var content strings.Builder
+	for _, badge := range badges {
+		fmt.Fprintf(&content, "\n## %s\n", badge.BadgeName)
+		fmt.Fprintf(&content, "%s\n", badge.BadgeDescription)
+		fmt.Fprintf(&content, "![%s](%s)\n", badge.BadgeName, badge.BadgeImageURL)
+		fmt.Fprintf(&content, "Link: [%s](%s)\n\n", badge.BadgeName, badge.BadgeURL)
+		content.WriteString("\n")
+	}
+	return content.String()
+}
+
+func replaceContentBetweenMarkers(existingContent string, badges []fetch.FilteredBadge) string {
+	startIndex := strings.Index(existingContent, startMarker)
+	endIndex := strings.LastIndex(existingContent, endMarker)
+
+	if startIndex == -1 || endIndex == -1 {
+		return fmt.Sprintf(defaultContent, generateBadgeContent(badges))
+	}
+
+	prefix := existingContent[:startIndex+len(startMarker)]
+	suffix := existingContent[endIndex:]
+	replacementContent := generateBadgeContent(badges)
+
+	return prefix + replacementContent + suffix
+}
 
 // ToFile a function that takes the result of fetch.Filterbadges and writes to file
 // named by user. It returns an error if any occurs.
@@ -31,13 +87,16 @@ const (
 //
 // ---
 func ToFile(filename string, badges []fetch.FilteredBadge) (bool, error) {
-	file, err := createOrOpenFile(filename)
+	filePath := filepath.Clean(fmt.Sprintf("%s.md", filename))
+
+	file, err := createOrOpenFile(filePath)
 	if err != nil {
 		return false, err
 	}
-	defer file.Close() //nolint: errcheck //no comment
+	defer file.Close()
 
-	err = replaceContent(file, badges)
+	bw := NewBadgeWriter(file)
+	err = bw.UpdateContent(file, badges)
 	if err != nil {
 		return false, err
 	}
@@ -45,105 +104,13 @@ func ToFile(filename string, badges []fetch.FilteredBadge) (bool, error) {
 	return true, nil
 }
 
-func createOrOpenFile(filename string) (*os.File, error) {
-	filePath := filepath.Clean(fmt.Sprintf("%s.md", filename))
-
-	// Check if the file exists
+func createOrOpenFile(filePath string) (*os.File, error) {
 	_, err := os.Stat(filePath)
 	if os.IsNotExist(err) {
-		// Create a new file if it doesn't exist
 		return os.Create(filePath)
 	} else if err != nil {
 		return nil, err
 	}
 
-	// Open the file for reading and writing
-	return os.OpenFile(filePath, os.O_RDWR, 0o600)
-}
-
-func replaceContent(file *os.File, badges []fetch.FilteredBadge) error {
-	// Read the existing file content
-	existingContent, err := readFileContent(file)
-	if err != nil {
-		return err
-	}
-
-	// Replace the content between the markers
-	updatedContent := replaceContentBetweenMarkers(existingContent, badges)
-
-	// Truncate the file and write the updated content
-	err = truncateAndWriteContent(file, updatedContent)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func readFileContent(file *os.File) (string, error) {
-	scanner := bufio.NewScanner(file)
-	var content strings.Builder
-	for scanner.Scan() {
-		content.WriteString(scanner.Text() + "\n")
-	}
-	if err := scanner.Err(); err != nil {
-		return "", err
-	}
-	return content.String(), nil
-}
-
-func replaceContentBetweenMarkers(existingContent string, badges []fetch.FilteredBadge) string {
-	startIndex := strings.Index(existingContent, startMarker)
-	endIndex := strings.LastIndex(existingContent, endMarker)
-
-	if startIndex == -1 || endIndex == -1 {
-		// If markers are not found, create the default content with markers
-		return fmt.Sprintf(defaultContent, generateBadgeContent(badges))
-	}
-
-	prefix := existingContent[:startIndex+len(startMarker)]
-	suffix := existingContent[endIndex:]
-	replacementContent := generateBadgeContent(badges)
-
-	return prefix + replacementContent + suffix
-}
-
-func generateBadgeContent(badges []fetch.FilteredBadge) string {
-	var content strings.Builder
-	for _, badge := range badges {
-		badgeInfo := []string{
-			fmt.Sprintf("\n## %s\n", badge.BadgeName),
-			fmt.Sprintf("%s\n", badge.BadgeDescription),
-			fmt.Sprintf("![%s](%s)\n", badge.BadgeName, badge.BadgeImageURL),
-			fmt.Sprintf("Link: [%s](%s)\n\n", badge.BadgeName, badge.BadgeURL),
-		}
-
-		for _, line := range badgeInfo {
-			content.WriteString(line)
-		}
-		content.WriteString("\n")
-	}
-	return content.String()
-}
-
-func truncateAndWriteContent(file *os.File, content string) error {
-	// Truncate the file to reset its content
-	err := file.Truncate(0)
-	if err != nil {
-		return err
-	}
-
-	// Seek to the beginning of the file
-	_, err = file.Seek(0, 0)
-	if err != nil {
-		return err
-	}
-
-	// Write the updated content to the file
-	_, err = file.WriteString(content)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return os.OpenFile(filePath, os.O_RDWR|os.O_TRUNC, 0o600)
 }
